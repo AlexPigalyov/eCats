8bd319b5d7a818da256ace78620b0dcec866a6ec
AlexPigalyov <alex.pigalyov@gmail.com>
2023-02-02 13:09:43 +0300

    remove label from logo.png + add adaptive to AppBarWidget.dart

--- lib/Account/Shared/AppBarWidget.dart
+++ lib/Account/Shared/AppBarWidget.dart
@@ -5,59 +5,62 @@ import '../../Models/Enums/PageEnum.dart';
 class AppBarWidget extends AppBar {
   final void Function(PageEnum) callback;
   AppBarWidget({super.key, required this.callback}):super (
-      toolbarHeight: 50,
-      backgroundColor: Colors.white,
-      foregroundColor: HexColor.fromHex('#8391a2'),
-      actions: [
-        Row(
-          children: [
-            Container(
-                margin: const EdgeInsets.only(right: 30),
-                alignment: FractionalOffset.centerLeft,
-                width: 150,
-                child: const Image(image: AssetImage('assets/logo.png'))
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                IconButton(
-                  iconSize: 32,
-                  icon: Image.asset('icons/flags/png/us.png',
-                      package: 'country_icons'),
-                  onPressed: () {},
-                ),
-                MaterialButton(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontFamily: 'Nunito',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: HexColor.fromHex('#8391a2'),
-                    ),
-                  ),
-                  onPressed: () {
-                    callback(PageEnum.Login);
-                  },
-                ),
-                MaterialButton(
-                  child: Text(
-                    "Register",
-                    style: TextStyle(
-                      fontFamily: 'Nunito',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: HexColor.fromHex('#8391a2'),
-                    ),
-                  ),
-                  onPressed: () {
-                    callback(PageEnum.Register);
-                  },
-                ),
-              ],
-            )
-          ],
+    toolbarHeight: 50,
+    backgroundColor: Colors.white,
+    foregroundColor: HexColor.fromHex('#8391a2'),
+    title: Flexible(
+      flex: 0,
+      fit: FlexFit.loose,
+      child: SizedBox(
+        width: 50,
+        child: Image.asset('assets/logo.png', fit: BoxFit.contain, height: 32,)
+      ),
+    ),
+    actions: <Widget>[
+      Flexible(
+        flex: 2,
+        fit: FlexFit.loose,
+        child: SizedBox(
+          child: IconButton(
+            icon: Image.asset('icons/flags/png/us.png', package: 'country_icons'),
+            onPressed: () {},
+          )
         )
-      ],
-    );
-  }
+      ),
+      Flexible(
+        flex: 3,
+        child: MaterialButton(
+          child: Text(
+            "Login",
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: HexColor.fromHex('#8391a2'),
+            ),
+          ),
+          onPressed: () {
+            callback(PageEnum.Login);
+          },
+        )
+      ),
+      Flexible(
+        flex: 3,
+        child: MaterialButton(
+          child: Text(
+            "Register",
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: HexColor.fromHex('#8391a2'),
+            ),
+          ),
+          onPressed: () {
+            callback(PageEnum.Register);
+          },
+        )
+      )
+    ]
+  );
+}
